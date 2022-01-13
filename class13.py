550a064beb8d22e6ba6afd6508af33dc7276a93a
Abssssss1753 <77192440+Abssssss1753@users.noreply.github.com>
2022-01-13 14:59:17 +0530

    initial commit

--- class13.py
+++ class13.py
@@ -1,33 +1,4 @@
-###################### IF ELSE ELIF statement ############################
-"""
-abhi = 10
-ravi = 20
-print("Enter the wait")
-john = int(input())
 
-if john>ravi:
-    print("bigger")
-elif john==ravi:
-    print("equal wait")
-else:
-    print("samller")
-"""
-"""
-
-################################ IN keyword ##################### 
-array = [2,4,6,8,10]
-print(10 in array)
-if 10 in array:
-    print("yes its in the array")
-"""
-
-"""
-########################  NOT IN keyword ##########################
-array = [2,4,6,8,10]
-print(15 not in array)
-if 15 not in array:
-    print("no its not in the array")
-"""
 ##################### DRIVING LICENSE(RTO) PROGRAME) ###########################
 
 cust1 = 18
